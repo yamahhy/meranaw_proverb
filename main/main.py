bc084ed9744e7d6daf6b2ca84218cf373f2c0ab6
Maha Ebrahim <hadjeebrahim.maha@s.msumain.wdu.ph>
2025-05-25 16:09:59 +0800

    Integration of Meranaw Interpreter

---
+++ main/main.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI, Query, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from typing import List
+import pandas as pd
+import numpy as np
+
+from transformers import pipeline
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+
+ # your firestore init function
+
+app = FastAPI()
+collection_name = "meranaw_proverbs"
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or your frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# --- INTERPRETATION MODEL ---
+
+model_path="./fine-tuned-t5-proverb-interpretation"
+generator = pipeline("text2text-generation", model=model_path)
+
+class InterpretRequest(BaseModel):
+    data: List # expects [meranaw, englishTranslation, interpretation]
+
+@app.post("/api/meranaw-interpreter")
+async def interpret_proverb(req: InterpretRequest):
+    meranaw, english_translation, interpretation = req.data  # <- Now expecting 3 values
+    prompt = f"Meranaw Proverb: {meranaw}\nEnglish Translation: {english_translation}\nInterpretation: {interpretation}\nInterpretation:"
+
+    result = generator(prompt, max_length=100, do_sample=True)
+    interpretation = result[0]['generated_text']
+
+    return {"data": [interpretation]}
